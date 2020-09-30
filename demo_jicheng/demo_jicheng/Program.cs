44f04b9bbb94e65b2d2fa0710d7d864289f13661
winner-guowei@163.com <15904045295Aa>
2020-09-30 10:35:24 +0800

    继承 is a

---
+++ demo_jicheng/demo_jicheng/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace demo_jicheng
+{
+
+    static class Program
+    {
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Type t = typeof(Car);
+            Type tb = t.BaseType;
+            Type tTop = tb.BaseType;
+
+            Car car = new Car();
+            // is a
+            Console.WriteLine(car is Vehicle);
+
+
+            Console.WriteLine(tb.FullName);
+        }
+    }
+
+    class Vehicle : Object
+    {
+
+    }
+
+    class Car: Vehicle
+    {
+
+    }
+}
