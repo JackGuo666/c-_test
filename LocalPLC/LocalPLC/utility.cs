538463cd74b230b109d30c6db04e3d1b8e3926df
wojiaoguowei <winner-guowei@163.com>
2020-11-18 10:35:36 +0800

    modbus起始地址

--- LocalPLC/LocalPLC/utility.cs
+++ LocalPLC/LocalPLC/utility.cs
@@ -13,6 +13,8 @@ namespace LocalPLC
         //modbus master的设备最大个数
         public static int masterDeviceCountMax = 8;
         public static int masterDeviceChannleCountMax = 8;
+        //modbus总起始地址
+        public static int modbusAddr = 1000;
         public static void PrintError(string str)
         {
             LocalPLC.UserControl1.multiprogApp.OutputWindows.Item("Infos").AddEntry(str, AdeOutputWindowMessageType.adeOwMsgInfo, "", "", 0, "");
