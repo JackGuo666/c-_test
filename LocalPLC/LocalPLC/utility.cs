5ac5b56186cdcccfa1258401f708972f017d4787
wojiaoguowei <winner-guowei@163.com>
2020-11-26 11:00:47 +0800

    提交

--- LocalPLC/LocalPLC/utility.cs
+++ LocalPLC/LocalPLC/utility.cs
@@ -30,39 +30,7 @@ namespace LocalPLC
          */
         public static void addIOGroups()
         {
-            IoGroups iog = LocalPLC.UserControl1.multiprogApp.ActiveProject.Hardware.Configurations.Item(1).Resources.Item(1).IoGroups;
-
-            int Count = iog.Count;
-
-
-            List<IoGroup> ll = new List<IoGroup>();
-            foreach (IoGroup ttt in iog)
-            {
-                var name = ttt.Name;
-                ll.Add(ttt);
-            }
-
-            foreach(var l in ll)
-            {
-                l.Delete();
-            }
-
-            var list = UserControl1.modmaster.masterManage.modbusMastrList;
-            foreach(var master in list)
-            {
-                string str = string.Format("master_in{0}", master.ID);
-
-                iog.Create(str, AdeIoGroupAccessType.adeIgatInput,
-            modbusMudule, "driver1", "<默认>", "", master.curMasterStartAddr, "test", AdeIoGroupDataType.adeIgdtByte,
-            1, 1, 1, 1);
-                str = string.Format("master_out{0}", master.ID);
-                iog.Create(str, AdeIoGroupAccessType.adeIgatOutput,
-                            modbusMudule, "driver1", "<默认>", "", master.curMasterStartAddr, "test", AdeIoGroupDataType.adeIgdtByte,
-                            1, 1, 1, 1);
-            }
-
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(iog);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(iog);
+            
         }
 
         public static void addVariables()
@@ -83,7 +51,9 @@ namespace LocalPLC
                 {
                     Variables variables = variablesObject as Variables;
                     var variable = variables.Create("test", "INT", AdeVariableBlockType.adeVarBlockVarGlobal,
-                                     "Inserted from AIFDemo", "12", "%MW3.1000", false);
+                                     "Inserted from AIFDemo", "12", "%MW3.100", false);
+                    variable = variables.Create("test1", "INT", AdeVariableBlockType.adeVarBlockVarGlobal,
+                                     "Inserted from AIFDemo", "12", "%IW1000", false);
 
                     variable.SetAttribute(20, 1);
                 }
