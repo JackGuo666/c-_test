6600e3b9b4a8565eab8c4326f3003376f71d2bd2
wojiaoguowei <winner-guowei@163.com>
2020-11-30 19:15:42 +0800

    slave变量自动变化

--- LocalPLC/LocalPLC/ModbusSlave/modbusslaveform.cs
+++ LocalPLC/LocalPLC/ModbusSlave/modbusslaveform.cs
@@ -9,8 +9,22 @@ using System.Windows.Forms;
 
 namespace LocalPLC.ModbusSlave
 {
+
+
     public partial class modbusslaveform : Form
     {
+        //00001至09999是离散输出(线圈)-----Coil status
+        private int modbusCoilStartAddr = 1;
+        //10001至19999是离散输入(触点)-----Input status
+        private int modbusDiscreteStartAddr = 10001;
+        private int modbusDiscreteEndAddr = 19999;
+
+        //30001至39999是输入寄存器(通常是模拟量输入)------Input register
+        private int modbusInputAddr = 30001;
+        //40001至49999是保持寄存器 -------Holding register
+        private int modbusHoldingStartAddr = 40001;
+        private int modbusHoldingEndAddr = 49999;
+
 
         private DataManager dataManager = null;
         ModbusSlaveData data_;
@@ -34,20 +48,93 @@ namespace LocalPLC.ModbusSlave
         private void textBox_coil_TextChanged(object sender, EventArgs e)
         {
             int.TryParse(textBox_coil.Text, out data_.dataDevice_.coilCount);
-            textBox_coil_start.Text = textBox_coil.Text;
+
+            if(textBox_coil_start.Text == "")
+            {
+                textBox_coil_start.Text = modbusCoilStartAddr.ToString();
+            }
+            else 
+            {
+                textBox_coil_start.Text = data_.dataDevice_.coilIoAddrStart.ToString();
+            }
+
+            int startAddr = 0;
+            int.TryParse(data_.dataDevice_.coilIoAddrStart, out startAddr);
+            if(data_.dataDevice_.coilCount + startAddr - 1 < modbusCoilStartAddr)
+            {
+                textBox_coil_end.Text = modbusCoilStartAddr.ToString();
+            }
+            else
+            {
+                textBox_coil_end.Text = (data_.dataDevice_.coilCount + startAddr - 1).ToString();
+            }
+            
         }
 
 
         private void textBox_holding_TextChanged(object sender, EventArgs e)
         {
             int.TryParse(textBox_holding.Text, out data_.dataDevice_.holdingCount);
+
+            if(textBox_holding_start.Text == "")
+            {
+                textBox_holding_start.Text = modbusHoldingStartAddr.ToString();
+            }
+
+
+            data_.dataDevice_.holdingIoAddrStart = textBox_holding_start.Text;
+
+            //结束地址
+            int startAddrTmp = 0;
+            int.TryParse(textBox_holding_start.Text, out startAddrTmp);
+            
+            if(startAddrTmp + data_.dataDevice_.holdingCount - 1 < modbusHoldingStartAddr)
+            {
+                textBox_holding_end.Text = modbusHoldingStartAddr.ToString();
+            }
+            else
+            {
+                textBox_holding_end.Text = (startAddrTmp + data_.dataDevice_.holdingCount - 1).ToString();
+            }
+
+            data_.dataDevice_.holdingIoAddrEnd = textBox_holding_end.Text;
         }
 
 
 
         private void textBox_lisan_TextChanged(object sender, EventArgs e)
         {
-            int.TryParse(textBox_lisan.Text, out data_.dataDevice_.decreteCount);
+            int decreteCount = 0;
+            int.TryParse(textBox_lisan.Text, out decreteCount);
+
+            if(textBox_lisan_start.Text == "")
+            {
+                textBox_lisan_start.Text = modbusDiscreteStartAddr.ToString();
+            }
+
+            if (textBox_lisan_end.Text == "")
+            {
+                textBox_lisan_end.Text = modbusDiscreteStartAddr.ToString();
+            }
+
+
+
+            int deceteAddrStart = 0;
+            int.TryParse(textBox_lisan_start.Text, out deceteAddrStart);
+            int deceteAddrEnd = deceteAddrStart + decreteCount - 1;
+            if(deceteAddrEnd < modbusDiscreteStartAddr | deceteAddrEnd > modbusDiscreteEndAddr)
+            {
+                return;
+            }
+
+            //起始
+            data_.dataDevice_.decreteIoAddrStart = deceteAddrStart.ToString();
+            //结束
+            data_.dataDevice_.decreteIoAddrEnd = deceteAddrEnd.ToString();
+
+            data_.dataDevice_.decreteCount = decreteCount;
+            textBox_lisan_end.Text = data_.dataDevice_.decreteIoAddrEnd;
+            textBox_lisan_start.Text = data_.dataDevice_.decreteIoAddrStart;
         }
 
 
@@ -58,12 +145,34 @@ namespace LocalPLC.ModbusSlave
 
         private void textBox_coil_start_TextChanged(object sender, EventArgs e)
         {
-                data_.dataDevice_.coilIoAddrStart = textBox_coil_start.Text  ;
+            data_.dataDevice_.coilIoAddrStart = textBox_coil_start.Text;
+
+            int startAddr = 0;
+            int.TryParse(data_.dataDevice_.coilIoAddrStart, out startAddr);
+            int.TryParse(textBox_coil.Text, out data_.dataDevice_.coilCount);
+            data_.dataDevice_.coilIoAddrEnd = (startAddr + data_.dataDevice_.coilCount - 1).ToString();
+            textBox_coil_end.Text = data_.dataDevice_.coilIoAddrEnd;
         }
 
         private void textBox_holding_start_TextChanged(object sender, EventArgs e)
         {
-            data_.dataDevice_.holdingIoAddrStart = textBox_holding_start.Text ;
+            int holdingStartAddr = 0;
+            int.TryParse(textBox_holding_start.Text, out holdingStartAddr);
+
+            if(holdingStartAddr < modbusHoldingStartAddr | holdingStartAddr > modbusHoldingEndAddr)
+            {
+                return;
+            }
+
+            int holdingEndAddr = holdingStartAddr + data_.dataDevice_.holdingCount - 1;
+            if (holdingEndAddr < modbusHoldingStartAddr | holdingEndAddr > modbusHoldingEndAddr)
+            {
+                return;
+            }
+
+            data_.dataDevice_.holdingIoAddrStart = textBox_holding_start.Text;
+            data_.dataDevice_.holdingIoAddrEnd = (holdingStartAddr + data_.dataDevice_.holdingCount - 1).ToString();
+            textBox_holding_end.Text = holdingEndAddr.ToString();
         }
 
         private void textBox_lisan_start_TextChanged(object sender, EventArgs e)
--- LocalPLC/LocalPLC/ModbusSlave/modbusslavemain.Designer.cs
+++ LocalPLC/LocalPLC/ModbusSlave/modbusslavemain.Designer.cs
@@ -32,10 +32,15 @@
             this.button_add = new System.Windows.Forms.Button();
             this.button_delete = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -46,7 +51,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 30;
-            this.dataGridView1.Size = new System.Drawing.Size(786, 396);
+            this.dataGridView1.Size = new System.Drawing.Size(381, 594);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
@@ -78,30 +83,53 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(786, 68);
+            this.panel1.Size = new System.Drawing.Size(1145, 70);
             this.panel1.TabIndex = 3;
             // 
-            // panel2
+            // splitContainer1
             // 
-            this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 68);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(786, 396);
-            this.panel2.TabIndex = 4;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 70);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView2);
+            this.splitContainer1.Size = new System.Drawing.Size(1145, 594);
+            this.splitContainer1.SplitterDistance = 381;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 30;
+            this.dataGridView2.Size = new System.Drawing.Size(760, 594);
+            this.dataGridView2.TabIndex = 5;
             // 
             // modbusslavemain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panel1);
             this.Name = "modbusslavemain";
-            this.Size = new System.Drawing.Size(786, 464);
+            this.Size = new System.Drawing.Size(1145, 664);
             this.Load += new System.EventHandler(this.modbusslavemain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -112,6 +140,7 @@
         private System.Windows.Forms.Button button_add;
         private System.Windows.Forms.Button button_delete;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
