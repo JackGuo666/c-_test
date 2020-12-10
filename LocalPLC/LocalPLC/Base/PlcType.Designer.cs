f4786979f5e9964d36911cac5738f101625fb7f2
wojiaoguowei <winner-guowei@163.com>
2020-12-10 18:45:41 +0800

    plc型号单击边框设置颜色

--- LocalPLC/LocalPLC/Base/PlcType.Designer.cs
+++ LocalPLC/LocalPLC/Base/PlcType.Designer.cs
@@ -74,6 +74,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(365, 280);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // PlcType
             // 
