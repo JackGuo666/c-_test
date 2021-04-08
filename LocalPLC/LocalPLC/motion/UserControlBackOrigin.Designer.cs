02235823378b8e9a421abef4d28209445d2344e0
wojiaoguowei <winner-guowei@163.com>
2021-04-08 18:25:52 +0800

    回原点输入限制

--- LocalPLC/LocalPLC/motion/UserControlBackOrigin.Designer.cs
+++ LocalPLC/LocalPLC/motion/UserControlBackOrigin.Designer.cs
@@ -31,15 +31,16 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.label32 = new System.Windows.Forms.Label();
             this.label25 = new System.Windows.Forms.Label();
+            this.comboBox_ZPulseSignal = new System.Windows.Forms.ComboBox();
+            this.comboBox_BackOriginal = new System.Windows.Forms.ComboBox();
             this.comboBox_BackOriginalSelectLevel = new System.Windows.Forms.ComboBox();
             this.label30 = new System.Windows.Forms.Label();
             this.label29 = new System.Windows.Forms.Label();
             this.label26 = new System.Windows.Forms.Label();
             this.label27 = new System.Windows.Forms.Label();
             this.label28 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.comboBox_BackOriginal = new System.Windows.Forms.ComboBox();
-            this.comboBox_ZPulseSignal = new System.Windows.Forms.ComboBox();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.button_valid = new System.Windows.Forms.Button();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +56,7 @@
             this.panel4.Controls.Add(this.label26);
             this.panel4.Controls.Add(this.label27);
             this.panel4.Controls.Add(this.label28);
-            this.panel4.Location = new System.Drawing.Point(3, 3);
+            this.panel4.Location = new System.Drawing.Point(6, 41);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(807, 115);
             this.panel4.TabIndex = 5;
@@ -78,6 +79,26 @@
             this.label25.TabIndex = 0;
             this.label25.Text = "回原点";
             // 
+            // comboBox_ZPulseSignal
+            // 
+            this.comboBox_ZPulseSignal.FormattingEnabled = true;
+            this.comboBox_ZPulseSignal.Location = new System.Drawing.Point(293, 72);
+            this.comboBox_ZPulseSignal.Name = "comboBox_ZPulseSignal";
+            this.comboBox_ZPulseSignal.Size = new System.Drawing.Size(141, 26);
+            this.comboBox_ZPulseSignal.TabIndex = 3;
+            this.comboBox_ZPulseSignal.SelectedIndexChanged += new System.EventHandler(this.comboBox_ZPulseSignal_SelectedIndexChanged);
+            this.comboBox_ZPulseSignal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox_ZPulseSignal_KeyPress);
+            // 
+            // comboBox_BackOriginal
+            // 
+            this.comboBox_BackOriginal.FormattingEnabled = true;
+            this.comboBox_BackOriginal.Location = new System.Drawing.Point(293, 38);
+            this.comboBox_BackOriginal.Name = "comboBox_BackOriginal";
+            this.comboBox_BackOriginal.Size = new System.Drawing.Size(141, 26);
+            this.comboBox_BackOriginal.TabIndex = 3;
+            this.comboBox_BackOriginal.SelectedIndexChanged += new System.EventHandler(this.comboBox_BackOriginal_SelectedIndexChanged);
+            this.comboBox_BackOriginal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox_BackOriginal_KeyPress);
+            // 
             // comboBox_BackOriginalSelectLevel
             // 
             this.comboBox_BackOriginalSelectLevel.FormattingEnabled = true;
@@ -85,6 +106,8 @@
             this.comboBox_BackOriginalSelectLevel.Name = "comboBox_BackOriginalSelectLevel";
             this.comboBox_BackOriginalSelectLevel.Size = new System.Drawing.Size(141, 26);
             this.comboBox_BackOriginalSelectLevel.TabIndex = 3;
+            this.comboBox_BackOriginalSelectLevel.SelectedIndexChanged += new System.EventHandler(this.comboBox_BackOriginalSelectLevel_SelectedIndexChanged);
+            this.comboBox_BackOriginalSelectLevel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox_BackOriginalSelectLevel_KeyPress);
             // 
             // label30
             // 
@@ -131,36 +154,35 @@
             this.label28.TabIndex = 0;
             this.label28.Text = "选择电平";
             // 
-            // richTextBox1
+            // button_cancel
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(3, 121);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(806, 434);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "图片";
+            this.button_cancel.BackColor = System.Drawing.Color.White;
+            this.button_cancel.Location = new System.Drawing.Point(126, 0);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(120, 35);
+            this.button_cancel.TabIndex = 8;
+            this.button_cancel.Text = "取消";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
             // 
-            // comboBox_BackOriginal
+            // button_valid
             // 
-            this.comboBox_BackOriginal.FormattingEnabled = true;
-            this.comboBox_BackOriginal.Location = new System.Drawing.Point(293, 38);
-            this.comboBox_BackOriginal.Name = "comboBox_BackOriginal";
-            this.comboBox_BackOriginal.Size = new System.Drawing.Size(141, 26);
-            this.comboBox_BackOriginal.TabIndex = 3;
-            // 
-            // comboBox_ZPulseSignal
-            // 
-            this.comboBox_ZPulseSignal.FormattingEnabled = true;
-            this.comboBox_ZPulseSignal.Location = new System.Drawing.Point(293, 72);
-            this.comboBox_ZPulseSignal.Name = "comboBox_ZPulseSignal";
-            this.comboBox_ZPulseSignal.Size = new System.Drawing.Size(141, 26);
-            this.comboBox_ZPulseSignal.TabIndex = 3;
+            this.button_valid.BackColor = System.Drawing.Color.White;
+            this.button_valid.Location = new System.Drawing.Point(0, 0);
+            this.button_valid.Name = "button_valid";
+            this.button_valid.Size = new System.Drawing.Size(120, 35);
+            this.button_valid.TabIndex = 9;
+            this.button_valid.Text = "应用";
+            this.button_valid.UseVisualStyleBackColor = true;
+            this.button_valid.Click += new System.EventHandler(this.button_valid_Click);
             // 
             // UserControlBackOrigin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.button_valid);
             this.Controls.Add(this.panel4);
             this.Name = "UserControlBackOrigin";
             this.Size = new System.Drawing.Size(813, 542);
@@ -181,8 +203,9 @@
         private System.Windows.Forms.Label label26;
         private System.Windows.Forms.Label label27;
         private System.Windows.Forms.Label label28;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ComboBox comboBox_ZPulseSignal;
         private System.Windows.Forms.ComboBox comboBox_BackOriginal;
+        private System.Windows.Forms.Button button_cancel;
+        private System.Windows.Forms.Button button_valid;
     }
 }
--- LocalPLC/LocalPLC/motion/UserControlBackOrigin.cs
+++ LocalPLC/LocalPLC/motion/UserControlBackOrigin.cs
@@ -23,6 +23,8 @@ namespace LocalPLC.motion
         {
             LocalPLC.Base.xml.DataManageBase dataManage = null;
             LocalPLC.UserControl1.UC.getDataManager(ref dataManage);
+            comboBox_BackOriginal.Items.Clear();
+            comboBox_ZPulseSignal.Items.Clear();
             foreach (var di in dataManage.diList)
             {
                 if (!di.used)
@@ -32,21 +34,105 @@ namespace LocalPLC.motion
                 }
             }
 
+            comboBox_BackOriginal.Text = data.axisMotionPara.backOriginal.orginInputSignal;
+            comboBox_ZPulseSignal.Text = data.axisMotionPara.backOriginal.ZPulseSignal;
+
             levelDic.Clear();
             levelDic.Add((int)TypeLevel.HIGH_LEVEL, "高电平有效");
             levelDic.Add((int)TypeLevel.LOW_LEVEL, "低电平有效");
+            comboBox_BackOriginalSelectLevel.Items.Clear();
             foreach (var level in levelDic)
             {
                 comboBox_BackOriginalSelectLevel.Items.Add(level.Value);
             }
+
+            comboBox_BackOriginalSelectLevel.SelectedIndex = data.axisMotionPara.backOriginal.selectLevel;
         }
 
-        public UserControlBackOrigin(TreeNode node_)
+        public UserControlBackOrigin(TreeNode node)
         {
             InitializeComponent();
 
 
+            if (node.Parent == null)
+            {
+                LocalPLC.utility.PrintInfo(string.Format("{0}节点没有父节点!", node.Parent));
+                return;
+            }
+
+            data = node.Parent.Tag as Axis;
+            node_ = node;
+
             initBackOriginal();
+
+            button_valid.Enabled = false;
+            button_cancel.Enabled = false;
+        }
+
+        private void comboBox_BackOriginal_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button_valid.Enabled = true;
+            button_cancel.Enabled = true;
+        }
+
+        private void comboBox_BackOriginalSelectLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button_valid.Enabled = true;
+            button_cancel.Enabled = true;
+        }
+
+        private void comboBox_ZPulseSignal_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button_valid.Enabled = true;
+            button_cancel.Enabled = true;
+        }
+
+        private void comboBox_BackOriginal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void comboBox_BackOriginalSelectLevel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void comboBox_ZPulseSignal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+
+        private void button_valid_Click(object sender, EventArgs e)
+        {
+            setDataFromUI();
+            button_valid.Enabled = false;
+            button_cancel.Enabled = false;
+        }
+
+        void setDataFromUI()
+        {
+            //回原点
+            var orginal = data.axisMotionPara.backOriginal;
+            orginal.orginInputSignal = comboBox_BackOriginal.Text;
+            orginal.selectLevel = comboBox_BackOriginalSelectLevel.SelectedIndex;
+            orginal.ZPulseSignal = comboBox_ZPulseSignal.Text;
+        }
+
+
+        void refreshData()
+        {
+            var original = data.axisMotionPara.backOriginal;
+            comboBox_BackOriginal.Text = original.orginInputSignal;
+            comboBox_BackOriginalSelectLevel.SelectedIndex = original.selectLevel;
+            comboBox_ZPulseSignal.Text = original.ZPulseSignal;
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            refreshData();
+            button_valid.Enabled = false;
+            button_cancel.Enabled = false;
         }
     }
 }
